fa30edbb388293d9d55c0216d4d2ca3926ec6298
Louie Bloomberg <basis-gyros8g@icloud.com>
2025-08-29 19:21:36 -0500

    Added a .0 to follow Semantic Versioning

--- secure_pwgen.py
+++ secure_pwgen.py
@@ -1,6 +1,6 @@
 # Louie Bloomberg
 # The Secure Password Generator!
-# Version 1.0
+# Version 1.0.0
 
 # Importing Modules: Links for things are mostly here with some other links scattered around.
 import random
